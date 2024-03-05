a0f9f67bb65e23fd4f6bd14bf8fac09e3444c66e
Sanuth23 <sanuth0000@gmail.com>
2024-03-05 14:16:10 +0530

    bug fix with service annotation

--- Backend/src/main/java/org/example/service/custom/impl/StaffServiceImpl.java
+++ Backend/src/main/java/org/example/service/custom/impl/StaffServiceImpl.java
@@ -6,6 +6,7 @@ import org.example.entity.StaffEntity;
 import org.example.repository.StaffRepository;
 import org.example.service.custom.StaffService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class StaffServiceImpl implements StaffService {
 
     @Autowired

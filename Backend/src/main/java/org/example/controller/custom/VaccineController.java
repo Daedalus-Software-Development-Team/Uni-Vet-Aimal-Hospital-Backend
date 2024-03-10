44717f5bd1a1a485d4e20ba6425cc3b493a67bc9
Sanuth23 <sanuth0000@gmail.com>
2024-03-10 16:39:36 +0530

    create vaccine controller class & methods

---
+++ Backend/src/main/java/org/example/controller/custom/VaccineController.java
@@ -0,0 +1,8 @@
+package org.example.controller.custom;
+
+import org.example.controller.CrudController;
+import org.example.dto.VaccineDto;
+import org.example.entity.VaccineEntity;
+
+public interface VaccineController extends CrudController<VaccineEntity, VaccineDto> {
+}
